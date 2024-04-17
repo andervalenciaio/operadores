477530619a6d561843c7e33320ffd80e9e8c1351
anderson valencia <vaanderson06@gmail.com>
2024-04-16 19:51:54 -0500

    operadores basicos aritmetricos

---
+++ ejercicios-operadores.js
@@ -0,0 +1,56 @@
+//ejemplo suma
+let resultadoSuma = 10 + 5;
+console.log(`la suma es: ${resultadoSuma}`); // Output: 15
+
+/*se declara la variable resultadosuma que es igual a 10 
+y el operador suma (+) 5, que da como resultado la suma de 
+los dos numeros para luego imprimirlos*/
+
+// ejemplo resta
+let resultadoResta = 20 - 7;
+console.log(`la resta es: ${resultadoResta}`); // Output: 13
+
+/*se declara la variable resultadoresta que es igual a 
+20 y el operador menos (-) 7, que nos da como resultado 
+la resta de los dos numeros para luego imprimirlos  */
+
+// ejemplo multiplicacion
+let resultadoMultiplicacion = 3 * 4;
+console.log(resultadoMultiplicacion); // Output: 12
+
+/*se declara la variable resultadoMultiplicacion que es igual a 
+3 y el operador multiplicacion (*) 4, que nos da como resultado 
+la multiplicacion  de los dos numeros para luego imprimirlos */
+
+//ejemplo division 
+let resultadoDivision = 16 / 4;
+console.log(`la division es: ${resultadoDivision}`); // Output: 4
+
+/* se declara la variable resultadoDivision que es igual a 
+16 y el operador division (/) 4, que nos da como resultado 
+la division  de los dos numeros para luego imprimirlos */
+
+//ejemplo residuo
+let resultadoResiduo = 10 % 3;
+console.log(`el residuo es: ${resultadoResiduo}`); // Output: 1
+
+/* se declara la variabke resultadoresiduo la cual es el residuo de
+10/3 lo que nos da como residuo 1*/
+
+// ejemplo incremento
+let numero = 1;
+numero++;
+console.log(`el incremento es: ${numero}`); // Output: 2
+
+/* se declara la variable numera, la cual es equivalente a 1, 
+luego se utiliza un operador de incremento, lo cual al momento
+de imprimir nos da 2*/
+
+//ejemplo de incremento
+let otroNumero = 5;
+otroNumero--;
+console.log(otroNumero); // Output: 4
+
+/* se declara la variable otronumero la cual es igual a 5
+luego se utiliza el operador de decremento el cual le va a restar 1
+a la variable, lo que al momento de imprimir nos da 4*/
